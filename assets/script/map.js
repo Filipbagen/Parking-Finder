60e329d56b0193581793ca6f432fc15bf95b6883
Filipbagen <filip.bagen@gmail.com>
2019-05-30 13:59:39 +0200

    Added possibility get current location

--- assets/script/map.js
+++ assets/script/map.js
@@ -19,9 +19,66 @@ const initMap = () => {
 
   parking1.setMap(map)
 
+  const ansgars = new google.maps.Polygon({
+    paths: ansgarsNorth,
+    strokeColor: '#18A718',
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#23E025',
+    fillOpacity: 0.4
+  })
+
+  map.addListener('center_changed', function () {
+    // 3 seconds after the center of the map has changed, pan back to the
+    // marker.
+    window.setTimeout(function () {
+      map.panTo(ansgars.getPosition());
+    }, 3000);
+  });
+
+  ansgars.addListener('click', function () {
+    map.setZoom(18);
+    map.setCenter(ansgars.getPosition());
+  });
+
+  ansgars.setMap(map)
+
   google.maps.event.addListener(parking1, 'click', dockUp)
 
   google.maps.event.addListener(map, 'click', dockDown)
+
+
+
+
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function () {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+
+
 }
 
 const getCurrentLocation = () => {
@@ -37,3 +94,6 @@ const dockDown = () => {
 }
 
 window['initMap'] = initMap
+
+const today = document.querySelector('#today')
+today.textContent = 'test'
---
+++ assets/script/openingHours.js
@@ -0,0 +1,4 @@
+const ansgars = [
+  'test',
+  'Gratis från 02:00-06:00',
+  'Gratis hela dagen']
